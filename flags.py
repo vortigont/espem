97e7622939a16b60cf053d90e812bc93d2d19302
Emil Muratov <gpm@hotplug.ru>
2021-05-17 16:15:37 +0300

    Fix pio building under windows
    replaced git version with python script
    Closes #8
    
    Signed-off-by: Emil Muratov <gpm@hotplug.ru>

---
+++ flags.py
@@ -0,0 +1,10 @@
+import subprocess
+
+# PIO build flags
+# https://docs.platformio.org/en/latest/projectconf/section_env_build.html#src-build-flags
+revision = (
+    subprocess.check_output(["git", "describe", "--abbrev=4", "--always", "--tags", "--long"])
+    .strip()
+    .decode("utf-8")
+)
+print("-DFW_VER='\"%s\"'" % revision)

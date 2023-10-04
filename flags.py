b252829052ab290af7f17ecfc6ca9a1d53ab180e
Emil Muratov <gpm@hotplug.ru>
2023-10-04 13:58:52 +0900

    update code to match EmbUI v2.8.1

--- flags.py
+++ flags.py
@@ -3,16 +3,15 @@ import subprocess
 
 # print("OS: %s" % os.name)
 if os.name == 'nt':
-  print("-DFW_VER='\"unknown\"'")
+  print("-DGIT_REV='\"n/a\"'")
   quit()
 
 
 # PIO build flags
 # https://docs.platformio.org/en/latest/projectconf/section_env_build.html#src-build-flags
-revision = (
-    subprocess.check_output(["git", "describe", "--abbrev=4", "--always", "--tags", "--long"])
-    .strip()
-    .decode("utf-8")
-)
-print("-DFW_VER='\"%s\"'" % revision)
-
+try:
+        #revision = subprocess.check_output(["git", "describe", "--abbrev=4", "--always", "--tags", "--long"], stderr=subprocess.DEVNULL).strip().decode("utf-8")
+        revision = subprocess.check_output(["git", "describe", "--abbrev=7", "--always"], stderr=subprocess.DEVNULL).strip().decode("utf-8")
+        print ("-DGIT_REV='\"%s\"'" % revision)
+except:
+        print ("-DGIT_REV='\"n/a\"'")
--- replace_fs.py
+++
@@ -1,9 +0,0 @@
-Import("env")
-import platform
-
-if platform.system()  == 'Windows':
-    print("Replace MKSPIFFSTOOL with mklittlefs.exe")
-    env.Replace (MKSPIFFSTOOL = "./mklittlefs.exe")
-else:
-    print("Replace MKSPIFFSTOOL with mklittlefs")
-    env.Replace (MKSPIFFSTOOL = "./mklittlefs")

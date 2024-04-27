7e542658531e552026981fd731a4f185ad34f58f
Emil Muratov <gpm@hotplug.ru>
2024-04-27 23:08:48 +0900

    update resources to match EmbUI v3.1.2

--- resources/respack.sh
+++ resources/respack.sh
@@ -4,7 +4,7 @@ USAGE="Usage: `basename $0` [-h] [-t embuitag] [-f]"
 
 # embui branch/tag name to fetch
 embuirepo='https://github.com/vortigont/EmbUI'
-embuitag="v3.1"
+embuitag="v3.1.2"
 
 #####
 # no changes below this point!
